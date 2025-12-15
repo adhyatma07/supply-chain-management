2a228757b214c60f3f3c5d74a75e7107335e7ab3
Adhyatma Hawale <adhyatmahawale1008@gmail.com>
2025-12-15 18:28:53 +0530

    Made some changes in the end points and Added @CrossOrigins annotation

--- src/main/java/org/jsp/SupplyChainManagement/Controller/CustomerController.java
+++ src/main/java/org/jsp/SupplyChainManagement/Controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.jsp.SupplyChainManagement.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//endpoint updated on 14-12-2025
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController 
--- src/main/java/org/jsp/SupplyChainManagement/Controller/ProductController.java
+++ src/main/java/org/jsp/SupplyChainManagement/Controller/ProductController.java
@@ -8,6 +8,7 @@ import org.jsp.SupplyChainManagement.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/product")
 public class ProductController 
